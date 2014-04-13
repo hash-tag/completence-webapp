506c2691d85883a01c7f54d5a19efbd5b3cd2e6b
saranath <saranath.karthik@gmail.com>
2014-04-13 12:06:23 -0700

    er:wq

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
-app.register('.html', require('jade'));
+var http = require('http'),
+    fs = require('fs');
 
-app.get('/', function(request, response) {
-  response.render('completence.html');
-});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+fs.readFile('./completence.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
